b705d423d6c1036425fee008a5f4a43f3e3e029b
Narasimha <narasimha@codebrahma.com>
2017-05-08 11:49:39 +0530

    Added Radio Button Component

---
+++ app/modules/UserAuthentication/components/Radio.js
@@ -0,0 +1,24 @@
+import React, { PropTypes } from 'react';
+
+const Radio = ({ className, name, title, value, checked}) => {
+	return (
+		<div className={className}>
+		  <input type="radio" name={name} value={value} defaultChecked={checked} />{title}
+		</div>
+	);
+};
+
+Radio.propTypes = {
+  className: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  checked: PropTypes.bool
+};
+
+Radio.defaultProps = {
+	className: '',
+	checked: false
+};
+
+export default Radio;
--- app/modules/UserAuthentication/components/index.js
+++ app/modules/UserAuthentication/components/index.js
@@ -13,18 +13,21 @@ import { HOC } from 'formsy-react';
 import Input from './Textfield';
 import MultilineInput from './Textarea';
 import Submit from './Submit';
+import Radio from './Radio';
 
 
 /*eslint-disable */
 
-const Textfield = HOC(Input);
-const Textarea = HOC(MultilineInput);
+const Textfield    = HOC(Input);
+const Textarea     = HOC(MultilineInput);
 const SubmitButton = HOC(Submit);
+const RadioButton  = HOC(Radio);
 
 /*eslint-enable */
 
 export {
   Textfield,
   Textarea,
-  SubmitButton
+  SubmitButton,
+  RadioButton
 };
